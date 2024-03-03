e55d1e4ccb73ff21018f24685cae1f75a1d0a662
thuongnguyen-spce <thuong.nguyen@space.com>
2024-03-03 12:21:05 +0700

    Add container deployment workflow and update webpack configuration

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const PackageJson = require("../package.json");
 
@@ -20,9 +19,6 @@ const devConfig = {
         marketings: "marketing@http://localhost:8081/remoteEntry.js",
       },
       shared: PackageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
