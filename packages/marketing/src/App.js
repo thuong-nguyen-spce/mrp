137a5e8e5bf0e8af5db17fc42dc201fefca6d6ec
thuongnguyen-spce <thuong.nguyen@space.com>
2024-03-04 22:16:00 +0700

    Fix routing issue and update webpack config

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, Router } from "react-router-dom";
 
 import {
   StylesProvider,
@@ -12,15 +12,15 @@ const generateClassName = createGenerateClassName({
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
-export default () => {
+export default ({ history }) => {
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Route exact path='/pricing' component={Pricing} />
           <Route path='/' component={Landing} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nexPathName }) {
+      const { pathname } = history.location;
+      if (pathname !== nexPathName) {
+        history.push(nexPathName);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const el = document.querySelector("#_marketing-dev-root");
 
   if (el) {
-    mount(el);
+    mount(el, { defaultHistory: createBrowserHistory() });
   }
 }
 
